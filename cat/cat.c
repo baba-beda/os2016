0cf4a47f5e34e9639a8849cfe30d8979be379b76
baba-beda <darya.nightmare@gmail.com>
2016-03-29 02:20:14 +0300

    defense01 commit

--- cat/cat.c
+++ cat/cat.c
@@ -12,20 +12,32 @@ void error_cat(const char* error_str) {
   exit(1);
 }
 
-int main() {
+int main(int argc, char * argv[]) {
   char buffer[4096];
   size_t read_cnt;
+  char * file = argv[1];
+  int input_fd = open(file, O_RDONLY);
+  if (input_fd == -1) {
+    error_cat(strerror(errno));
+  }
+  
   do {
-    read_cnt = (size_t) read(STDIN_FILENO, buffer, sizeof(buffer));
-    if (read_cnt == -1) {
+    read_cnt = (size_t) read(input_fd, buffer, sizeof(buffer));
+    if (read_cnt == -1 && EINTR != errno) {
       error_cat(strerror(errno));
 
     }
-    size_t write_cnt = (size_t) write(STDOUT_FILENO, buffer, read_cnt);
-    if (write_cnt < read_cnt) {
-      error_cat("Unexpected EOF");
-    }
-  } while (read_cnt > 0);
+    size_t write_cnt;
+    size_t written = 0;
+    do {
+      write_cnt = (size_t) write(STDOUT_FILENO, buffer, read_cnt - written);
+      if (write_cnt == -1 && EINTR != errno) {
+        error_cat(strerror(errno));
+      }
+      written += write_cnt;
+    } while (written != read_cnt);
+
+  } while (read_cnt > 0 && EINTR == errno);
 
   return 0;
 }
